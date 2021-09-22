1c64f9766ebb0bf98319d8edc494e4565078590d
Tim Petrvalsky <tpet55@yahoo.com>
2021-09-22 16:22:24 -0700

    Create node app and add a few routes

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+let notes = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456'
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523'
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345'
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122'
+	}
+];
+
+const info = {
+	message: `phonebook has info for ${notes.length} people`,
+	date: new Date()
+};
+
+app.get('/', (request, response) => {
+	response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/persons', (request, response) => {
+	response.json(notes);
+});
+
+app.get('/info', (request, response) => {
+	response.json(info);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
