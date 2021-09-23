30e80d7365366c1d36c70b1d9c599bdc5df31ccf
Tim Petrvalsky <tpet55@yahoo.com>
2021-09-23 11:45:26 -0700

    Finish node notes app

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ let notes = [
 	}
 ];
 
+app.use(express.json());
+
 const info = {
 	message: `phonebook has info for ${notes.length} people`,
 	date: new Date()
@@ -35,6 +37,61 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
 	response.json(notes);
+});
+
+app.post('/api/persons', (request, response) => {
+	const randomId = Math.floor(Math.random() * 10000);
+	const person = request.body;
+	console.log(person, 'REQUEST.BODY');
+
+	const duplicateName = notes.find((item) => {
+		return item.name === request.body.name;
+	});
+
+	if (!person.name) {
+		return response.status(400).json({
+			error: 'Name is missing'
+		});
+	} else if (!person.number) {
+		return response.status(400).json({
+			error: 'Number is missing'
+		});
+	} else if (duplicateName) {
+		return response.status(400).json({
+			error: 'That name already exists'
+		});
+	}
+
+	const newPerson = {
+		id: randomId,
+		name: request.body.name,
+		number: request.body.number
+	};
+	console.log(randomId, 'RANDOMID');
+	notes = notes.concat(newPerson);
+	response.json(notes);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+	const personId = Number(request.params.id);
+	const note = notes.find((note) => note.id === personId);
+
+	if (note) {
+		response.json(note);
+	} else {
+		response.status(404).end();
+	}
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+	const personId = Number(request.params.id);
+	const persons = notes.filter((note) => note.id !== personId);
+
+	if (persons) {
+		response.json(persons);
+	} else {
+		response.status(204).end();
+	}
 });
 
 app.get('/info', (request, response) => {
